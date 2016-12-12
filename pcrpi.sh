7ad36395339efeef62707a9381e2eb9d239d43d6
Maximilian Friedersdorff <max@friedersdorff.com>
2016-12-12 11:06:06 +0000

    Tool config and script to run PCRPI
    Tool runs PCRPI and watches the log file to determine if
    and when it terminated successfully.

---
+++ pcrpi.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+PCRPI="/interactomix/bin-tools/PCRPI/run.pl"
+ECHO="/bin/echo"
+GREP="/bin/grep"
+SED="/bin/sed"
+WATCH="/bin/watch"
+CP="/bin/cp"
+
+run_output=$(sg interactomix -c "$PCRPI $1 $2 $3 $4")
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+
+output_dir=$($ECHO "${run_output}" | $GREP "Output dir" | $SED "s/^Output\ dir\ \.\.\.\ //")
+
+$WATCH -g $GREP "'Finish submission at'" "${output_dir}/run.sge.log" 1>/dev/null
+
+for f in "run.sge.log" "input.org.pdb" "input.clean.pdb" "b_factors.pdb" \
+	"results.txt" "contacts.csu.txt"; do
+	if [ ! -e ${output_dir}/$f ]; then
+		exit 1
+	fi
+done
+
+$CP ${output_dir}/results.txt $5
