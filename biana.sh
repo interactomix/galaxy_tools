88749cc35936c2a0d85210339b57902d9a1933c2
Maximilian Friedersdorff <max@friedersdorff.com>
2017-02-08 11:51:52 +0000

    Fix path to biana executable

--- biana.sh
+++ biana.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-BIANA="/interactomix/bin-tools/BIANA/run.pl"
+BIANA="/interactomix/bin-tools/BIANA/generate_netscore_files.py"
 
 sg interactomix -c "$BIANA -iseed $1  -radius $2 -stype $3 -ttype $4 \
   -taxid $5 $6 -node $7 -edge $8 -trans $9"
