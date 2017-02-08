c901b02ee2459c30f359256dce655e7e9c053ee5
Maximilian Friedersdorff <max@friedersdorff.com>
2017-02-08 17:01:49 +0000

    Set pythonpath

--- biana.sh
+++ biana.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 BIANA="/interactomix/bin-tools/BIANA/generate_netscore_files.py"
 
-sg interactomix -c "python2 $BIANA -iseed $1  -radius $2 -stype $3 -ttype $4 \
+sg interactomix -c "PYTHONPATH=$PYTHONPATH:/interactomix/bin/BIANA python2 $BIANA \
+  -iseed $1  -radius $2 -stype $3 -ttype $4 \
   -taxid $5 $6 -node $7 -edge $8 -trans $9"
