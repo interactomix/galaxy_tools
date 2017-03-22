d62389dc93a862c82872398fc607192080b88ed3
Maximilian Friedersdorff <max@friedersdorff.com>
2017-03-22 11:47:08 +0000

    Move biana script into xml

--- biana.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-BIANA="/interactomix/bin-tools/BIANA/generate_netscore_files.py"
-
-sg interactomix -c "PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/site-packages:/interactomix/bin/BIANA python2 $BIANA \
-  -iseed $1  -radius $2 -stype $3 -ttype $4 \
-  -taxid $5 -node $6 -edge $7 -trans $8 $9"
