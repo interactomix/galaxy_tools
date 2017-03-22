3910fedb8f55d824efda5a393d4be5b3f8c74979
Maximilian Friedersdorff <max@friedersdorff.com>
2017-03-22 11:39:40 +0000

    Pass optional parameter last

--- biana.sh
+++ biana.sh
@@ -3,4 +3,4 @@ BIANA="/interactomix/bin-tools/BIANA/generate_netscore_files.py"
 
 sg interactomix -c "PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/site-packages:/interactomix/bin/BIANA python2 $BIANA \
   -iseed $1  -radius $2 -stype $3 -ttype $4 \
-  -taxid $5 $6 -node $7 -edge $8 -trans $9"
+  -taxid $5 -node $6 -edge $7 -trans $8 $9"
