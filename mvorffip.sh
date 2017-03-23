c98cbf6413074681f10984c618b94406bd0dd3dd
Maximilian Friedersdorff <max@friedersdorff.com>
2017-03-23 13:32:19 +0000

    Fix bugs in the mvorffip wrapper

--- mvorffip.sh
+++ mvorffip.sh
@@ -18,6 +18,7 @@ $CP $1 $tmp_pdbfile
 
 cd $(dirname ${tmp_pdbfile})
 run_output=$(sg interactomix -c "PERL5LIB=$PERL5LIB $PERL $MVORFFIP $(basename $tmp_pdbfile) 2> /dev/null")
+echo $run_output
 if [ $? -ne 0 ]; then
 	exit 1
 fi
@@ -31,15 +32,25 @@ ebs_tab_result="${output_dir}/XLS/ebs_prediction.tab"
 dbs_tab_result="${output_dir}/XLS/dbs_prediction.tab"
 rbs_tab_result="${output_dir}/XLS/rbs_prediction.tab"
 
-pbs_result=$(ls "${output_dir}/PDB/RESULT/*.pbs.pdb")
-ebs_result=$(ls "${output_dir}/PDB/RESULT/*.ebs.pdb")
-dbs_result=$(ls "${output_dir}/PDB/RESULT/*.dbs.pdb")
-rbs_result=$(ls "${output_dir}/PDB/RESULT/*.rbs.pdb")
+pbs_result=$(ls "${output_dir}"/PDB/RESULT/*_pbs.pdb)
+ebs_result=$(ls "${output_dir}"/PDB/RESULT/*_ebs.pdb)
+dbs_result=$(ls "${output_dir}"/PDB/RESULT/*_dbs.pdb)
+rbs_result=$(ls "${output_dir}"/PDB/RESULT/*_rbs.pdb)
 
 for f in $pbs_tab_result $ebs_tab_result $dbs_tab_result $rbs_tab_result $pbs_result $ebs_result $dbs_result $rbs_result; do
-  if [ -f "${f}"]; then
-    cp "${f}" "$2"
+  if [ -f "${f}" ]; then
+    true
   else
     exit 1
   fi
 done
+
+$CP $pbs_tab_result $2
+$CP $ebs_tab_result $3
+$CP $dbs_tab_result $4
+$CP $rbs_tab_result $5
+
+$CP $pbs_result $6
+$CP $ebs_result $7
+$CP $dbs_result $8
+$CP $rbs_result $9
