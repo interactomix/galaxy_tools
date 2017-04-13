291f56e0297c0d0456c3f2ffec8eb0298849af25
Maximilian Friedersdorff <max@friedersdorff.com>
2017-04-13 13:56:05 +0100

    Push logic into separate srcipt for vd2ock

---
+++ vd2ock.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+tmp_outdir = $(mktemp -d /interactomix/output-tools/VD2DOCK/XXXXXX)
+
+PERL5LIB="/interactomix/bin-tool/VD2OCK:$PERL5LIB" perl run_vd2ock.pl \
+	$1 \
+	$2 \
+	$(dirname ${tmp_outdir}) \
+	$(basename ${tmp_outdir}) \
+	$3 \
+	$4 \
+
+cat $(ls ${tmp_outdir}/out_rot_* | grep -Ev -e ".*\.sp" -e ".*\.zr\.out") > $5
+cat ${tmp_outdir}/out_rot*.sp > $6
+cat ${tmp_outdir}/out_rot*.zr.out > $7
